7faccc9e698f59c5b37de4452bbaab83069fc731
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-26 16:33:22 -0300

    feat (basic services): implements getData() and uses FS module.

--- src/services/Base.service.ts
+++ src/services/Base.service.ts
@@ -1,1 +1,22 @@
+import fs from "fs";
+import { Entity } from "../interfaces/core";
 
+export const getData = <T extends Entity>(file: string, id?: number) => {
+  const fileContent: T[] = JSON.parse(
+    fs.readFileSync(file) as unknown as string
+  );
+  let data;
+  if (id) {
+    console.log("id", id);
+    data = fileContent.filter((item) => item.id === id)[0];
+  } else {
+    data = fileContent;
+  }
+  return data;
+};
+
+export const postData = () => {};
+
+export const putData = () => {};
+
+export const deleteData = () => {};
