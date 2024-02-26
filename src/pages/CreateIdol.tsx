01ca37fa6ece45f186f1caa76b6f221db89bbd02
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-26 15:45:45 -0300

    feat (CreateIdol.tsx): adds styles and child component.

--- src/pages/CreateIdol.tsx
+++ src/pages/CreateIdol.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { IdolForm } from "../components";
 
 const CreateIdol = () => {
-  return <div>CreateIdol</div>;
+  return (
+    <div>
+      <div
+        style={{
+          fontWeight: 600,
+          fontSize: 24,
+          textAlign: "center",
+          justifyContent: "center",
+          padding: 16,
+        }}
+      >
+        CreateIdol
+      </div>
+      <div style={{ gap: 4 }}>
+        <IdolForm actions={[<button>Create idol</button>]} />
+      </div>
+    </div>
+  );
 };
 
 export default CreateIdol;
