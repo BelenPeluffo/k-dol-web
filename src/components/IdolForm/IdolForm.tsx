42743f6a76578b0b702c5d6caece477886a483d3
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-26 15:46:26 -0300

    feat (IdolForm.tsx): adds styles, updates props interface.

--- src/components/IdolForm/IdolForm.tsx
+++ src/components/IdolForm/IdolForm.tsx
@@ -1,19 +1,46 @@
-import { zodiacSigns } from "../../constants";
-import { ZodiacSign } from "../../interfaces/core";
+import { ReactNode } from "react";
+import { useGenericForm } from "../hooks";
 
-const IdolForm = () => {
-  const createFormField = (fields: [], onChange: () => void, intialValues: []) => {
-    return fields.map((field) => (
-      <select name={field} id={field} title={field}>
-        {zodiacSigns.map((zodiacSign: ZodiacSign) => (
-          <option value={zodiacSign.id} key={zodiacSign.id}>
-            {zodiacSign.name}
-          </option>
-        ))}
-      </select>
-    ));
-  };
-  return <div>IdolForm</div>;
+const IdolForm = ({ actions }: { actions?: ReactNode[] }) => {
+  const { createPositionsFormField } = useGenericForm();
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: 8,
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: 4,
+        }}
+      >
+        {createPositionsFormField()}
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          gap: 4,
+          padding: 8,
+        }}
+      >
+        {actions}
+      </div>
+    </div>
+  );
 };
 
 export default IdolForm;
