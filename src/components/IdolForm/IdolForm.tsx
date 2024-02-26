02922bdd286b0ad05445e3cfc9c0a4c40193189f
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-26 15:06:00 -0300

    feat (IdolForm): creates util to creat dropdown menues.

--- src/components/IdolForm/IdolForm.tsx
+++ src/components/IdolForm/IdolForm.tsx
@@ -1,5 +1,18 @@
+import { zodiacSigns } from "../../constants";
+import { ZodiacSign } from "../../interfaces/core";
+
 const IdolForm = () => {
-  const createFormField = () => {};
+  const createFormField = (fields: [], onChange: () => void, intialValues: []) => {
+    return fields.map((field) => (
+      <select name={field} id={field} title={field}>
+        {zodiacSigns.map((zodiacSign: ZodiacSign) => (
+          <option value={zodiacSign.id} key={zodiacSign.id}>
+            {zodiacSign.name}
+          </option>
+        ))}
+      </select>
+    ));
+  };
   return <div>IdolForm</div>;
 };
 
