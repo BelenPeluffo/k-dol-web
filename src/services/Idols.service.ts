75edf24a88ff75ad753eac2eaa7b755accddb0d8
Belén Peluffo <belen.peluffo@dirmod.com>
2024-02-27 15:20:24 -0300

    feat (Idol Service): implements update method. Remove and update methods function weirdly. To resolve when implementing API.

--- src/services/Idols.service.ts
+++ src/services/Idols.service.ts
@@ -15,17 +15,23 @@ export class IdolService implements IdolInterface {
   }
   add(idol: Idol): void {
     console.log("Adding idol:", idol.name);
-    console.log('idol?', idol);
-    this.db.push({...idol, id: this.db.length + 1});
+    console.log("idol?", idol);
+    this.db.push({ ...idol, id: this.db.length + 1 });
   }
   remove(id: number): Idol[] {
     // const newData = mockData.filter((idol) => idol.id !== id);
     // mockData = newData;
-    this.db.splice(id, 1);
+    const newData = this.db.filter((idol) => idol.id !== id);
+    this.db = newData;
     console.log("this.db?", this.db);
     return this.db;
   }
-  update(idol: Idol): void {
-    throw new Error("Method not implemented.");
+  update(idol: Idol): Idol[] {
+    const {id} = idol;
+    const newData = this.db.filter((idol) => idol.id !== id);
+    this.db = newData;
+    console.log("this.db before pushing?", this.db);
+    this.db.push(idol);
+    return this.db;
   }
 }
